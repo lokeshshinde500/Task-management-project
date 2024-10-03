ef7b903c6c4572cb26272185b7c94a0d34c6b0d0
Lokesh shinde <lokeshinde500@gmail.com>
2024-10-03 06:42:52 +0530

    add mongodb cloud link

--- Server/src/config/constant.js
+++ Server/src/config/constant.js
@@ -3,8 +3,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 3000,
-  DB_URL: process.env.DB_LOCAL,
-  // DB_URL: process.env.DB_URL,
+  DB_URL: process.env.DB_URL,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   MY_EMAIL: process.env.MY_EMAIL,
   SMTP_PASSWORD: process.env.SMTP_PASSWORD,
